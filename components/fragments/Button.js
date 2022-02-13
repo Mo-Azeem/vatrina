11e52c481295df4c65b2dd794fd8bf73ab85d4dc
MoeIsLearning <moeislearning@gmail.com>
2022-02-13 16:49:02 +0200

    commit

--- components/fragments/Button.js
+++ components/fragments/Button.js
@@ -1,15 +1,17 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Button({ lable, onClick, logo }) {
   return (
     <div
-      className="button cursor-pointer rounded-xl bg-primary px-16 py-2 font-bold text-gray-50"
+      className="button bg-primary cursor-pointer rounded-xl px-12 py-2 font-bold text-gray-50"
       onClick={() => onClick()}
     >
       <div className="content flex flex-row items-center">
-      <p>{lable}</p>
-      {logo && <div className='splitter h-8 border-l-[1px] mx-4 border-gray-300 opacity-50'></div>}
-      {logo && <Image src={logo} width='20px' height='20px' alt='logo'/>}
+        <p>{lable}</p>
+        {logo && (
+          <div className="splitter mx-4 h-8 border-l-2 border-gray-300 opacity-50"></div>
+        )}
+        {logo && <Image src={logo} width="20px" height="20px" alt="logo" />}
       </div>
     </div>
   );
